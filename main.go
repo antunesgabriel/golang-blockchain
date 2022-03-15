fe38bca6c98577b7dcf1bcef64e2cdc88092cd7e
Gabriel Antunes <gabriel.tantunes@b2wdigital.com>
2022-03-15 16:42:33 -0300

    example how to use that blockchain

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"github.com/antunesgabriel/golang-blockchain/domain"
 )
 
 func main() {
-	fmt.Println("Hello word")
+	genesisData := map[string]string{
+		"To":    "Genesis",
+		"Value": "67.9",
+	}
+
+	genesis := domain.NewBlock(genesisData, "0")
+
+	blockchain := domain.NewBlockChain(genesis)
+
+	data := map[string]string{
+		"From":    "Genesis",
+		"To":      "Antunes",
+		"Value":   "1.0",
+		"Message": "Great Job!",
+	}
+
+	lastBlock := blockchain.GetLastBlock()
+
+	block := domain.NewBlock(data, string(lastBlock.Hash))
+
+	blockchain.AddBlock(block)
+
 }
